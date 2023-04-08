42d4b7c57c606b7f571abc47148763ad312d04ac
ninojoey <44876783+ninojoey@users.noreply.github.com>
2023-04-08 01:23:55 -0700

    program that'll stores online json values into dictionaries
    refers to justin's json files to grab students' ID and Name, then throws them into a bidriection dictionary. and does it again but with items

---
+++ StudentItemInfo.py
@@ -0,0 +1,93 @@
+import json
+import requests
+
+
+
+
+
+CHAR_JSON_URL = "https://raw.githubusercontent.com/JustinL163/BA-Resource-Planner/main/planner/json/charlist.json"
+ITEM_JSON_URL = "https://raw.githubusercontent.com/JustinL163/BA-Resource-Planner/main/planner/json/localisations.json"
+
+
+
+class bidict:
+    def __init__(self):
+        self.dict = {}
+        self.revDict = {}
+        
+    def __init__(self, dictionary):
+        self.dict = dictionary
+        self.revDict = {}
+        for key, value in dictionary.items():
+            self.revDict[value] = key
+
+    def add(self, key, value):
+        self.dict[key] = value
+        self.revDict[value] = key
+
+    def get(self, key):
+        if key in self.dict:
+            return self.dict[key]
+        elif key in self.revDict:
+            return self.revDict[key]
+        else:
+            return "Key does not exist"
+        
+
+
+
+
+
+# get the dictionary of studentID:studentInfo
+resp = requests.get(CHAR_JSON_URL)
+studentsDict =  json.loads(resp.text)
+
+
+studentBidict = bidict({})
+studentIDList = []
+studentNameList = []
+
+for studentID in studentsDict:
+    studentInfo = studentsDict[studentID]
+    if "Name" in studentInfo:
+        studentName = studentInfo["Name"]
+        
+        studentBidict.add(studentID, studentName)
+        studentIDList.append(studentID)
+        studentNameList.append(studentName)
+
+
+
+
+
+
+# this json is  first split into languages. we want the english one
+# then it's split into items and characters. we want only the items
+
+resp = requests.get(ITEM_JSON_URL)
+langLocals =  json.loads(resp.text)
+categories = langLocals["En"]
+itemDict = categories["Items"]
+
+itemBidict = bidict({})
+itemIDList = []
+itemNameList = []
+
+for itemID in itemDict:
+    itemInfo = itemDict[itemID]
+    if "Name" in itemInfo:
+        itemName = itemInfo["Name"]
+        
+        itemBidict.add(itemID, itemName)
+        itemIDList.append(itemID)
+        itemNameList.append(itemName)
+    else:
+        if itemID in studentIDList:
+            studentIndex = studentIDList.index(itemID)
+            itemName = "{}'s Eleph".format(studentNameList[studentIndex])
+
+            itemBidict.add(itemID, itemName)
+            itemIDList.append(itemID)
+            itemNameList.append(itemName)
+                                
+        
